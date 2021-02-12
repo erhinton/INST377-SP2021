8a3f7e24b15aec6a049876d5668d04ffbb8cc6a3
erhinton <65618575+erhinton@users.noreply.github.com>
2021-02-12 14:57:25 -0500

    ADD: carousel complete

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,21 @@
-/* Put your javascript in here */
+const width = 130;
+const count = 3;
+
+const list = carousel.querySelector('ul');
+const listElems = carousel.querySelectorAll('li');
+
+let position = 0;
+
+carousel.querySelector('.left').onclick = function() {
+  position += width * count;
+
+  position = Math.min(position, 0);
+  list.style.marginLeft = `${position}px`;
+};
+
+carousel.querySelector('.right').onclick = function() {
+  position -= width * count;
+
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = `${position}px`;
+};
