dc78a2f4ee09e439d6aafd732714c34723a19965
erhinton <65618575+erhinton@users.noreply.github.com>
2021-02-19 18:24:14 -0500

    ADD: finished lab 4

--- server.js
+++ server.js
@@ -42,8 +42,7 @@ app.route('/api')
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    console.log('Now send something back to your client');
-    // res.json({data: dataToSendToFrontEnd});
+    res.send('Hello World')
   });
 
 app.listen(port, async () => {
