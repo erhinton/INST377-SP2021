d28e5b257f231b661cb4f3de0da41ed21b345f80
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-09 15:48:35 -0500

    Added Map and partially working search bar

--- public/labs/lab_5/assingmentScript.js
+++ public/labs/lab_5/assingmentScript.js
@@ -1,19 +1,4 @@
-function mapInit() {
-  const map = L.map('mapid').setView([38.9897, -76.9378], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZWhpbnRvbiIsImEiOiJja20yZ21rcGUxNHJyMm9yM3Q4dDE1dGFjIn0.-83mPUmqtwJV5j2Z8RftvQ'
-  }).addTo(map);
-  console.log('map',map)
-  return map;
-}
-
-
-async function SearchFilter() {
+async function windowAction() {
  
   const request = await fetch('/api')
   const restaurants = await request.json()
@@ -48,25 +33,12 @@ async function SearchFilter() {
   }
 
   const searchInput = document.querySelector(".SearchBar");
-  const suggestions = document.querySelector(".ListOfLocations");
+  const suggestions = document.querySelector(".ListOfRestaurants");
 
-
-  document.querySelector("button").addEventListener("click", (e) => {
+  searchInput.addEventListener("keyup", (e) => {
       console.log('LOADING...');
     displayMatches(e);
   });
 }
 
-
-
-async function dataHandler(mapObjectFromFunction) {
-  SearchFilter()
-  
-}
-
-async function windowActions() {
-  const map = mapInit();
-  await dataHandler(map);
-}
-
-window.onload = windowActions;
+window.onload = windowAction;
